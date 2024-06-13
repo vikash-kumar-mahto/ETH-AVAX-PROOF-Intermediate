13d1d6d59f1251398745ee3bf1236eab102f1b37
vikash kumar mahto <93486699+vikash-kumar-mahto@users.noreply.github.com>
2024-06-14 00:04:25 +0530

    Update MyToken.sol

--- Module_3/MyToken.sol
+++ Module_3/MyToken.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract VikashToken is ERC20 {
+    address public owner;
+
+    constructor(uint256 initialSupply) ERC20("vikash", "VIKASH") {
+        _mint(msg.sender, initialSupply);
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Error: You are not the owner");
+        _;
+    }
+
+    function mint(address to, uint256 value) external onlyOwner {
+        _mint(to, value);
+    }
+
+    function burn(address from, uint256 value) external {
+        _burn(from, value);
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+}
